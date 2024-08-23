608e1ec33eacefe089ca61dab52cdebd7f8776f9
Dota43ver <facundoagueroc@gmail.com>
2024-08-23 11:06:26 -0300

    rest terminado

--- src/main/java/com/contactos/rest/contactos/rest/ContactoREST.java
+++ src/main/java/com/contactos/rest/contactos/rest/ContactoREST.java
@@ -1,5 +1,43 @@
 package com.contactos.rest.contactos.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.contactos.rest.contactos.model.Contacto;
+import com.contactos.rest.contactos.service.ContactoService;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/contactos/")
 public class ContactoREST {
 
+    @Autowired
+    private ContactoService contactoService;
+
+    @GetMapping
+    private ResponseEntity<List<Contacto>> getAllContactos(){
+        return ResponseEntity.ok(contactoService.findAll());
+    }
+
+    @PostMapping
+    private ResponseEntity<Contacto> saveContacto(@RequestBody Contacto contacto){
+        Contacto contactoGuardado = contactoService.save(contacto);
+        try {
+            return ResponseEntity.created(new URI("/contactos/"+ contactoGuardado.getId())).body(contactoGuardado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        
+    }
 }
--- src/main/java/com/contactos/rest/contactos/service/EstadoService.java
+++ src/main/java/com/contactos/rest/contactos/service/EstadoService.java
@@ -1,18 +1,13 @@
 package com.contactos.rest.contactos.service;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Function;
+
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+
 import org.springframework.stereotype.Service;
 
 import com.contactos.rest.contactos.model.Estado;
